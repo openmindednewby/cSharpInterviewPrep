91f6c1a11c70cfdd4b8fd7a84bd21e28f937af2d
Demetrios Loizides <loizidesdemetris@gmail.com>
2025-11-23 22:46:57 +0200

    Add CreateLeaveTypeCommand and handler implementation
    Added a `ProjectReference` to the `Domain` project in the
    `Application.csproj` file and removed an unused `<Folder>` entry.
    
    Introduced the `CreateLeaveTypeCommand` class to represent the
    data required for creating a leave type. Implemented the
    `CreateLeaveTypeHandler` class to handle the command, leveraging
    MediatR, AutoMapper, and the `ILeaveTypeRepository` for mapping
    and persistence. The handler asynchronously creates a leave type
    and returns its ID.

---
+++ exampleSolutions/TrevoirWilliamsCourse/Application/Features/LeaveType/Commands/CreateLeaveType/CreateLeaveTypeCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Features.LeaveType.Commands.CreateLeaveType
+{
+    public class CreateLeaveTypeCommand : IRequest<int>
+    {
+        public string Name { get; set; } = string.Empty;
+        public int DefaultDays { get; set; }
+
+    }
+}
---
+++ exampleSolutions/TrevoirWilliamsCourse/Application/Features/LeaveType/Commands/CreateLeaveType/CreateLeaveTypeHandler.cs
@@ -0,0 +1,28 @@
+﻿using Application.Contracts.Persistence;
+using AutoMapper;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Features.LeaveType.Commands.CreateLeaveType
+{
+    public class CreateLeaveTypeHandler : IRequestHandler<CreateLeaveTypeCommand, int>
+    {
+        private readonly IMapper _mapper;
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+        public CreateLeaveTypeHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository)
+        {
+            _mapper = mapper;
+            _leaveTypeRepository = leaveTypeRepository;
+        }
+
+        public async Task<int> Handle(CreateLeaveTypeCommand request, CancellationToken cancellationToken)
+        {
+            var leaveTypeToCreate = _mapper.Map<Domain.LeaveType>(request);
+            await _leaveTypeRepository.CreateAsync(leaveTypeToCreate);
+            return leaveTypeToCreate.Id;
+        }
+    }
+}
